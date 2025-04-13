fa945493e06db921dcf97544ad5c4cbe2df87fb9
Devesh Sharma <p24cs0001@iitj.ac.in>
2025-04-13 08:01:00 -0700

    simpleclick: Make changes to run with latest pytorch
    Modified the code to run it with latest cuda and pytorch
    
    Signed-off-by: Devesh Sharma <p24cs0001@iitj.ac.in>

--- run_evaluate.sh
+++ run_evaluate.sh
@@ -1,7 +1,7 @@
-MODEL_PATH=./weights/simpleclick_models/cocolvis_vit_base.pth
+MODEL_PATH=./weights/weights/cocolvis_vit_base.pth
 
 python scripts/evaluate_model.py NoBRS \
 --gpu=0 \
 --checkpoint=${MODEL_PATH} \
 --eval-mode=cvpr \
---datasets=GrabCut
+--datasets=GrabCut,Berkeley,DAVIS,SBD --iou-analysis --vis-preds --print-ious --n-clicks=10
